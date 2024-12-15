eb04c2701b4509ebff566d311beda9cc34e6735c
exo-pt <rafael.rodrgs@gmail.com>
2024-12-15 18:34:18 +0000

    Version 1.1.0
    - Added interaction to plots
    - Added new lc's author (GSFC-ELEANOR-LITE)
    - Added parameters of the star
    . Some minor bugs corrected

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # TESS Lightcurves app
 
-Tess lightcurve viewer for all available sectors of a target in TIC catalog<br/>
-This is a small webapp currently hosted in Streamlit Community Cloud.
+Tess lightcurve viewer for all available sectors of a target in TIC catalog<br/><br/>
+This is a small webapp currently hosted in Streamlit Cloud. You can access it at<br/>
+https://tess-lightcurves.streamlit.app
+<br/>
+or start the webapp locally in a Python env:
+> streamlit run Tess_Lightcurves.py
+The app will open in the default browser
 
-For a selected TIC number, one lightcurve is displayed for each available sector.
-If  more than one author available for a sector, the displayed lightcurve, in availability order, is:
-&nbsp;&nbsp; SPOC (2 min cadence) -> TESS-SPOC -> QLP.
+For a selected TIC number, the lightcurve of each sector available on MAST, is displayed.
+If  more than one author is available for a single sector, the displayed lightcurve, in availability order, is:
+&nbsp;&nbsp; SPOC (2 min) -> TESS-SPOC -> QLP -> ELEANOR.
 
-The user can choose between PDCSAP and SAP flux 
+The user can choose between PDCSAP and SAP flux (for SPOC and TESS-SPOC), and can interact with the plots (thx to [Plotly](https://github.com/plotly/plotly.py) python library)
 
 ![Image](https://github.com/exo-pt/Tess-Lightcurves-app/blob/main/Tess-Lightcurves-app.png?raw=true)
 
 ## Dependencies:
 - numpy
 - pandas
-- matplotlib
 - lightkurve
+- astroquery
+- plotly
 - streamlit
-
-
-## Contributors
-Rafael Rodrigues
--- src/Tess_Lightcurves/Tess_Lightcurves.py
+++ src/Tess_Lightcurves/Tess_Lightcurves.py
@@ -2,38 +2,76 @@ import logging,warnings
 import lightkurve as lk
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 import streamlit as st
+from astroquery.mast import Catalogs
 from version import __version__
 
+def set_css():
+    st.markdown("""
+        <style>
+            .js-plotly-plot .plotly, .js-plotly-plot .plotly div {
+                margin-bottom: -25px;
+            }
+            .spc{
+                font-size: 13px;
+                padding-bottom: 0.2rem;
+            }
+            .stFormSubmitButton button{
+                float:right;
+                color: navy;
+            }
+            div[data-testid="stExpander"] details summary p{
+                font-size: 1.1rem;
+            }
+            div[data-testid="stExpander"] summary{
+                padding: 0.15rem 0.5rem;
+            }
+            div[data-testid="stSidebar"]{
+                width: 320px !important;
+            }
+            div[data-testid="stSidebarUserContent"] h1{
+                font-size: 2.7rem;
+                text-align: center;
+            }
+            .credits, a, a:visited {
+                font-size: 0.8rem;
+                text-align: center;
+            }
+            a, a:visited {
+                color: navy;
+                font-weight:bold;
+            }
+
+            div[data-testid="stMainBlockContainer"] {
+                max-width: 90rem;
+                padding: 1.75rem 2.5rem 0 2.5rem;
+            }
+            div[data-testid="stMarkdownContainer"] p{
+                font-size: 1.1rem;
+            }
+            div[data-testid="stVerticalBlock"]{
+                gap: 0rem;
+            }
+            h1{
+                padding-bottom: 0.3rem;
+            }
+        </style>
+        """, unsafe_allow_html=True)
+
+
 if __name__ == '__main__':
+    st.set_page_config(page_title="Tess Lightcurves app", layout="wide")
+    set_css()
     try:
         xticid = st.query_params.tic #  ?tic=165795955
     except:
         xticid = ''
-    st. set_page_config(layout="wide")
-    st.markdown("""
-        <style>
-           .block-container {
-                padding-top: 1rem;
-            }
-            div[data-testid="stExpander"] details summary p{
-                font-size: 1.1rem;
-                color: navy;
-            }
-            div[data-testid="stSidebarUserContent"] h1{
-                font-size: 2.5rem;
-                text-align: center;
-            }
-            div[data-testid="stMarkdownContainer"] p{
-                font-size: 1.1rem;
-                color: navy;
-            }
-        </style>
-        """, unsafe_allow_html=True)
+
     ticid = 0
     with st.sidebar:
         st.title('**TESS Lightcurves**')
+        st.html('&nbsp;')
         with st.form("my_form"):
             tic = st.text_input('**TIC number:**', value=xticid, placeholder='', max_chars=10)
             if tic != '':
@@ -41,74 +79,126 @@ if __name__ == '__main__':
                     ticid = int(tic)
                 except ValueError:
                     ticid=0
+            st.html('&nbsp;')
             tipo = st.selectbox(
-                '**Flux type:**',
+                '**Flux type** (spoc/tess-spoc):',
                 ('PDCSAP flux', 'SAP flux')
             )
-            st.form_submit_button("**:blue[PLOT]**")
+            st.html('<div class="credits">&nbsp;</div>')
+            st.form_submit_button('**PLOT**')
+        #st.html('&nbsp;')
         st.html('<div align="right">v'+__version__+'</div>')
+        st.markdown('<div class="credits">Github source code: <a href="https://github.com/exo-pt/Tess-Lightcurves-app">Tess Lightcurves</a><br>Using <a href="https://github.com/lightkurve/lightkurve">Lightkurve</a>' +\
+                ' and <a href="https://github.com/plotly/plotly.py">Plotly</a> Python packages.</div>', unsafe_allow_html=True)
 
     if ticid != 0:
         TICstr = 'TIC '+ str(ticid)
         st.title(TICstr)
+        st.html("<style>[data-testid='stHeaderActionElements'] {display: none;}</style>")
+        cat=[]
+        res=''
+        try:
+            cat = Catalogs.query_object(TICstr, radius=0.0003, catalog="TIC")
+        except:
+            try:
+                cat = Catalogs.query_object(TICstr, radius=0.0003, catalog="TIC")
+            except:
+                st.error('Could not resolve '+ TICstr)
+                st.stop()
+
+        cat['rad'] = cat['rad'].round(3)
+        cat['ra'] = cat['ra'].round(5)
+        cat['dec'] = cat['dec'].round(5)
+        cat['Tmag'] = cat['Tmag'].round(2)
+        #cat['priority'] = cat['priority'].round(4)
+        #cat['contratio'] = cat['contratio'].round(4)
+        cat['d'] = cat['d'].round(2)
+        cat['logg'] = cat['logg'].round(3)
+        rho = round(cat['rho'][0]*1.41, 3)
+
+        linha = '&nbsp;<b>RA</b>: ' + str(cat['ra'][0]) + '  .  <b>Dec</b>:' + str(cat['dec'][0]) + '  .  <b>Tmag</b>: ' + str(cat['Tmag'][0]) +'  .  <b>Rad</b>: ' + str(cat['rad'][0]) +  \
+            '  .  <b>Mass</b>: ' + str(cat['mass'][0]) + '  .  <b>Teff</b>: ' + str(cat['Teff'][0]) + '  .  <b>Logg</b>: '+ str(cat['logg'][0]) + '  .  <b>MH</b>: '+ str(cat['MH'][0])+'  .  <b>rho</b>: ' + \
+            str(round(rho,5)) +  '  .  <b>Dist</b>(pc): ' +str(cat['d'][0])+'<br/>'
+        linha = linha.replace('nan', '?')
+        linha = linha.replace(' . ', '&nbsp;&nbsp;&nbsp;')
+        st.html('<div class="spc">' + linha +'</div>')
         try:
             res=lk.search_lightcurve(TICstr, mission='TESS')
         except:
             res = ''
         if len(res) == 0:
             if res=='':
-                st.write('Error in lk.search_lightcurve... Try again.')
+                st.error('Error in lk.search_lightcurve... Try again.')
             else:
-                st.write('No available lightcurves or TIC number error...')
-        else:
-            df = res.table.to_pandas()
-            sectors = df['sequence_number'].drop_duplicates().sort_values().to_list()
-            sec_spoc = df[df['provenance_name'] == 'TESS-SPOC']['sequence_number'].sort_values().to_list()
-            sec_2min = df[df['exptime'] == 120]['sequence_number'].sort_values().to_list()
-            sec_qlp = df[df['provenance_name'] == 'QLP']['sequence_number'].sort_values().to_list()
+                st.error('No available lightcurve data at MAST from SPOC, TESS_SPOC, QLP or ELEANOR.')
+            st.stop()
 
-            txt = '**Observed Sectors: ' + str(sectors) + '**'
-            with st.expander(txt):
-                txt = 'SPOC: <b>' + str(sec_2min) + '</b><br>TESS-SPOC: <b>' + str(sec_spoc) + '</b>'
-                txt = txt + '<br>QLP: <b>' + str(sec_qlp) + '</b>'
-                st.html(txt)
+        df = res.table.to_pandas()
+        sectors = df['sequence_number'].drop_duplicates().sort_values().to_list()
 
-            warnings.simplefilter("ignore")
-            logging.getLogger("lightkurve").setLevel(logging.ERROR)
+        sec_spoc = df[df['provenance_name'] == 'TESS-SPOC']['sequence_number'].drop_duplicates().sort_values().to_list()
+        sec_2min = df[df['provenance_name'] == 'SPOC']['sequence_number'].drop_duplicates().sort_values().to_list()
+        sec_qlp = df[df['provenance_name'] == 'QLP']['sequence_number'].drop_duplicates().sort_values().to_list()
+        sec_eleanor = df[df['provenance_name'].str.contains("ELEANOR")]['sequence_number'].drop_duplicates().sort_values().to_list()
 
-            sectors.reverse()
-            for sec in sectors:
-                try:
-                    if sec in sec_2min:
-                        if tipo == 'SAP flux':
-                            lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='SPOC', exptime='120').download(flux_column='sap_flux').remove_outliers(sigma_lower=10, sigma_upper=3).normalize().remove_nans()
-                        else:
-                            lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='SPOC', exptime='120').download().remove_outliers(sigma_lower=10, sigma_upper=3).normalize().remove_nans()
-                    else:
-                        if sec in sec_spoc:
-                            if tipo == 'SAP flux':
-                                lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='TESS-SPOC').download(flux_column='sap_flux').remove_outliers(sigma_lower=10, sigma_upper=3).normalize().remove_nans()
-                            else:
-                                lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='TESS-SPOC').download().remove_outliers(sigma_lower=20, sigma_upper=3).normalize().remove_nans()
-                        else:
-                            if sec in sec_qlp:
-                                lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='QLP').download(quality_bitmask=1073749231).remove_outliers(sigma_lower=20, sigma_upper=3).normalize().remove_nans()
-                            else:
-                                continue
-                except:
-                    st.write('Error reading lightcurve sector '+ sec)
-                    continue
+        sec_spoc = df[df['provenance_name'] == 'TESS-SPOC']['sequence_number'].drop_duplicates().sort_values().to_list()
+        sec_2min = df[df['exptime'] == 120]['sequence_number'].drop_duplicates().sort_values().to_list()
+        sec_qlp = df[df['provenance_name'] == 'QLP']['sequence_number'].drop_duplicates().sort_values().to_list()
 
-                fig = plt.figure(figsize=(15, 2.4))
-                plt.margins(x=0.02) #, y=0.05)
+        txt = '**Available Sectors: ' + str(sectors) + '**'
+        with st.expander(txt):
+            table = '<table><tr><td>SPOC: </td><td>'+ str(sec_2min) + '</td></tr>' +\
+                '<tr><td>TESS-SPOC: </td><td>'+ str(sec_spoc) + '</td></tr>' +\
+                '<tr><td>QLP: </td><td>'+ str(sec_qlp) + '</td></tr>' +\
+                '<tr><td>ELEANOR: </td><td>'+ str(sec_eleanor) + '</td></tr></table>'
+            #txt = 'SPOC: <b>' + str(sec_2min) + '</b><br>TESS-SPOC: <b>' + str(sec_spoc) + '</b>'
+            #txt = txt + '<br>QLP: <b>' + str(sec_qlp) + '</b><br>ELEANOR: <b>' + str(sec_eleanor)
+            st.html(table)
+        st.html('&nbsp;')
+        warnings.simplefilter("ignore")
+        logging.getLogger("lightkurve").setLevel(logging.ERROR)
+        sectors.reverse()
+        tot = 0
+
+        for sec in sectors:
+            tot += 1
+            try:
                 if sec in sec_2min:
-                    plt.title('Sector ' + str(sec) + ' (SPOC)', loc='left')
-                else:
-                    if sec in sec_spoc:
-                        plt.title('Sector ' + str(sec) + ' (TESS-SPOC)', loc='left')
+                    tit = 'Sector ' + str(sec) + ' (SPOC)'
+                    if tipo == 'SAP flux':
+                        lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='SPOC', exptime='120').download(flux_column='sap_flux').remove_outliers(sigma_lower=20, sigma_upper=3).normalize().remove_nans()
                     else:
-                        plt.title('Sector ' + str(sec) + ' (QLP)', loc='left')
-                plt.scatter(lc0.time.value, lc0.flux.value, s=1, color='darkslategrey')
-                plt.minorticks_on()
-                plt.grid(color='lightgrey', linewidth=0.6)
-                st.pyplot(fig)
+                        lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='SPOC', exptime='120').download().remove_outliers(sigma_lower=20, sigma_upper=3).normalize().remove_nans()
+                elif sec in sec_spoc:
+                    tit = 'Sector ' + str(sec) + ' (TESS-SPOC)'
+                    if tipo == 'SAP flux':
+                        lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='TESS-SPOC').download(flux_column='sap_flux').remove_outliers(sigma_lower=20, sigma_upper=3).normalize().remove_nans()
+                    else:
+                        lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='TESS-SPOC').download().remove_outliers(sigma_lower=20, sigma_upper=3).normalize().remove_nans()
+                elif sec in sec_qlp:
+                    tit = 'Sector ' + str(sec) + ' (QLP)'
+                    lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='QLP').download(quality_bitmask=1073749231).remove_outliers(sigma_lower=20, sigma_upper=3).normalize().remove_nans()
+                elif sec in sec_eleanor:
+                    tit = 'Sector ' + str(sec) + ' (ELEANOR)'
+                    lc0 = lk.search_lightcurve(TICstr, sector=sec, mission='TESS', author='GSFC-ELEANOR-LITE').download().remove_outliers(sigma_lower=20, sigma_upper=3).normalize().remove_nans()
+                else:
+                    continue
+            except:
+                st.write(':red[Error] reading '+ tit + '(try again...)')
+                continue
+
+            df = lc0.to_pandas().reset_index()
+            df = df[['time', 'flux']]
+            fig = px.scatter(df, x='time', y='flux', color_discrete_sequence=['darkslategrey'])
+            fig.update_traces(marker={'size': 3.3})
+            minx = min(df['time']) - 0.7
+            maxx = max(df['time']) + 0.7
+            fig.update_xaxes(range=[minx-0.05, maxx+0.05])
+            fig.add_vrect(x0=minx, x1=maxx, line_width=0.5)
+            fig.update_layout(title=dict(text=tit, y=0.9, font=dict(size=17, color='grey')))
+            fig.update_layout(yaxis = dict(tickfont = dict(size=13), tickformat = '.4f'), xaxis = dict(tickfont = dict(size=13)))
+            fig.update_layout(yaxis_title=None, xaxis_title=None)
+            fig.update_xaxes(showgrid=True, gridcolor='#ddd',title_standoff = 1, minor=dict(ticklen=4, tickcolor="grey", nticks=5))
+            fig.update_yaxes(showgrid=True, gridcolor='#ddd')
+            fig.update_layout(height=285)
+            st.plotly_chart(fig, use_container_width=False)
